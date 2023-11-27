8c1671b53403dbacce5d569ad8578cb465e11e69
rusakaraleon@gmail.com <rusakaraleon@gmail.com>
2023-11-27 08:34:47 -0200

    ne

--- src/components/Cards/MessageNotification.jsx
+++ src/components/Cards/MessageNotification.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const MessageNotification = ({ sender, message, timestamp }) => {
   return (
     <div className="message-card">
-      <div className="message-sender">{sender}</div>
+      <div className="message-sender"><b>{sender}</b></div>
       <div className="message-content">{message}</div>
       <div className="message-timestamp">{timestamp}</div>
     </div>
